3b015f47e790d3aeffb3780c43b31f572a92d14c
Mario Eduardo Domínguez Meléndez <mario.ed.domm@gmail.com>
2020-03-12 12:39:00 -0600

    cabios openpay pruebas y menus

---
+++ WSViajes/WSViajes/Comunes/OpenPayFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Openpay;
+using Openpay.Entities;
+
+namespace WSViajes.Comunes
+{
+    public class OpenPay
+    {
+        OpenpayAPI openpayAPI;
+
+        public OpenPay()
+        {
+            openpayAPI = new OpenpayAPI("sk_a896b738bdc541a599e75c52df080bb4", "mes0ipoqxgvxnuyu4kpg", false);
+        }
+
+        public void createCustomer()
+        {
+            Customer customer = new Customer();
+            customer.Name = "Net Client";
+            customer.LastName = "C#";
+            customer.Email = "net@c.com";
+            customer.Address = new Address();
+            customer.Address.Line1 = "line 1";
+            customer.Address.PostalCode = "12355";
+            customer.Address.City = "Queretaro";
+            customer.Address.CountryCode = "MX";
+            customer.Address.State = "Queretaro";
+
+            Customer customerCreated = openpayAPI.CustomerService.Create(customer);
+        }
+    }
+}
+
+
